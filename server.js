74a6d06ad2df22498cd0c8b34faeafe7e004cafc
Erycsmart <102563239+Erycsmart@users.noreply.github.com>
2026-03-22 19:36:59 +0300

    Initialize Express server with basic route

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server working 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running on " + PORT));
