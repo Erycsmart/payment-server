a9352a4c16345bd966ac171ec14263c924e785ce
Erycsmart <102563239+Erycsmart@users.noreply.github.com>
2026-03-22 20:13:43 +0300

    Add home and test routes to server

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+// Home route
 app.get("/", (req, res) => {
   res.send("Server working 🚀");
+});
+
+// Test route
+app.get("/test", (req, res) => {
+  res.send("Server + env working ✅");
 });
 
 const PORT = process.env.PORT || 3000;
